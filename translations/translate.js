aa24a4be948e147d2ed4a46791041906c49d894e
Steve Seguin <steve@seguin.email>
2020-09-09 02:58:34 -0400

    did I fix it?

--- translations/translate.js
+++ translations/translate.js
@@ -1,7 +1,7 @@
 // Copy and paste this code into OBS.Ninja's developer's console to generate new Translation files
 
-function downloadTranslation(filename, trans){  // downloads the current translation to a file
-	document.querySelectorAll('[data-translate]').forEach(function(ele){
+function downloadTranslation(filename, trans={}){  // downloads the current translation to a file
+	allItems.forEach(function(ele){
 		trans[ele.dataset.translate] = ele.innerHTML;
 	});
 
@@ -12,46 +12,54 @@ function downloadTranslation(filename, trans){  // downloads the current transla
 	hiddenElement.target = '_blank';
 	hiddenElement.download = filename+".json";
 	hiddenElement.click();
+	return trans;
 }
 
 
 function updateTranslation(filename){  // updates the website with a specific translation
-	return fetch("./translations/"+filename+'.json').then(function(response){
-		if (response.status !== 200) {
-			log('Looks like there was a problem. Status Code: ' +
-			response.status);
-			return false;
+	var request = new XMLHttpRequest();
+	request.open('GET', "./translations/"+filename+'.json?'+(Math.random()*100).toString(), false);  // `false` makes the request synchronous
+	request.send(null);
+
+	if (request.status !== 200) {
+	  return;
+	}
+	try{
+		data = JSON.parse(request.responseText);
+	} catch(e){
+		console.log(request.responseText);
+		console.error(e);
+		return false;
+	}
+	allItems.forEach(function(ele){
+		if (ele.dataset.translate in data){
+			ele.innerHTML = data[ele.dataset.translate];
 		}
-		return response.json().then(function(data) {
-			document.querySelectorAll('[data-translate]').forEach(function(ele){
-				if (ele.dataset.translate in data){
-					ele.innerHTML = data[ele.dataset.translate];
-				}
-			});
-			try{
-				getById("mainmenu").style.opacity = 1;;
-			} catch(e){}
-			return [filename, data];
-		}).catch(function(err){
-			errorlog(err);
-			try{
-				getById("mainmenu").style.opacity = 1;
-			} catch(e){}
-			return false;
-		});
 	});
+	return [filename, data];
 }
 
-var updateList = ["en", "de", "es", "ru", "fr", "pl",  "ja", "ar", "it", "nl", "pt", "zh", "blank"];  // list of languages to update. Update this if you add a new language.
+var updateList = ["en", "de", "es", "ru", "fr", "pl", "ja", "ar", "it", "nl", "pt"];  // list of languages to update. Update this if you add a new language.
+var allItems = document.querySelectorAll('[data-translate]');
+var defaultTrans = {};
+allItems.forEach(function(ele){
+	defaultTrans[ele.dataset.translate] = ele.innerHTML;
+});
 
-//downloadTranslation("default");
+console.log(defaultTrans);
 
 for (var i in updateList){
 	var ln = updateList[i];
-	updateTranslation(ln).then(function(res){
-		if (res[0]){
-			console.log(res[0]);
-			downloadTranslation(res[0], res[1]);
+	res = updateTranslation(ln);
+	if (res==false){continue;}
+	if (res[0]){
+		console.log(res[0]);
+		downloadTranslation(res[0], res[1]);
+	}
+	
+	allItems.forEach(function(ele){
+		if (ele.dataset.translate in defaultTrans){
+			ele.innerHTML = defaultTrans[ele.dataset.translate];
 		}
-	}); 
+	});
 }
