d0e0d7e04d987d877509a5ee95e720a8f4048d37
Steve Seguin <steve@seguin.email>
2023-11-13 20:23:44 -0500

    comms/mixer app + electronCapture elevated priv support
    electron capture might not have worked with the mixer or comms app when in elevateed privs. Fixed the issue I think

--- thirdparty/jquery/jquery-3.6.0.js
+++ thirdparty/jquery/jquery-3.6.0.js
@@ -15,27 +15,8 @@
 
 	"use strict";
 
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-
-		// For CommonJS and CommonJS-like environments where a proper `window`
-		// is present, execute the factory and get jQuery.
-		// For environments that do not have a `window` with a `document`
-		// (such as Node.js), expose a factory as module.exports.
-		// This accentuates the need for the creation of a real `window`.
-		// e.g. var jQuery = require("jquery")(window);
-		// See ticket #14549 for more info.
-		module.exports = global.document ?
-			factory( global, true ) :
-			function( w ) {
-				if ( !w.document ) {
-					throw new Error( "jQuery requires a window with a document" );
-				}
-				return factory( w );
-			};
-	} else {
-		factory( global );
-	}
-
+	factory( global );
+	
 // Pass this if window is not defined yet
 } )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
 
