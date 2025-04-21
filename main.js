c54299876f0180928292761f158d125a77a23416
bca-admin <admin@belarusians.ca>
2025-04-21 12:34:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -104,11 +104,13 @@ async function main() {
 	if (urlParams.has("room")) {
 		getById("container-2").classList.remove("hidden");
 		getById("container-3").classList.remove("hidden");
+		getById("bg").classList.add("hidden");
 		console.log("in the room: " + urlParams.get("room"));
 	}
 	else{
 		getById("container-2").classList.add("hidden");
 		getById("container-3").classList.add("hidden");
+		getById("bg").classList.remove("hidden");
 	}
 	
 	if (urlParams.has("cleanoutput") || urlParams.has("clean") || urlParams.has("cleanish")) {
